01c68d4dbcd4393a932f1cade051d8f806126f51
Jovan <jovanvidovic96@gmail.com>
2026-05-16 19:25:05 +0200

    feat: initial project setup - Spring Boot + React

---
+++ battlebord/src/main/java/com/dnd/battlebord/BattlebordApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.battlebord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlebordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlebordApplication.class, args);
+	}
+
+}
---
+++ battlebord/src/test/java/com/dnd/battlebord/BattlebordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.battlebord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlebordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
