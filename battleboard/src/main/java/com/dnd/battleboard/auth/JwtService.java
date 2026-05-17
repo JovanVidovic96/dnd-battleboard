20ad467b4afa267c16f483bf021afddc617f2bff
Jovan <jovanvidovic96@gmail.com>
2026-05-17 16:32:40 +0200

    feat: JWT, register, login endpoints working

--- battleboard/src/main/java/com/dnd/battleboard/auth/JwtService.java
+++ battleboard/src/main/java/com/dnd/battleboard/auth/JwtService.java
@@ -2,19 +2,17 @@ package com.dnd.battleboard.auth;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
-
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "cYVCiGw7Kdo3Wx5bcsB8NmEbbcCEESZ5N7h800vkjVg=";
     private static final long EXPIRATION_MS = 1000 * 60 * 60 * 24;
-
 
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
@@ -23,15 +21,15 @@ public class JwtService {
 
     public String generateToken(String username) {
         return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(getSigningKey())
                 .compact();
     }
 
     public String extractUsername(String token) {
-        return  extractClaims(token).getSubject();
+        return extractClaims(token).getSubject();
     }
 
     public boolean isTokenValid(String token) {
@@ -42,12 +40,11 @@ public class JwtService {
         }
     }
 
-    private Claims extractClaims( String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) getSigningKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
-
